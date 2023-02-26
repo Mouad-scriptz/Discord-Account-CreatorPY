22af7489fe2969de2ec136ef5db8e3cf11618acb
Mouad scriptz <102869034+Mouad-scriptz@users.noreply.github.com>
2023-02-26 21:48:00 +0100

    organized print functions

--- modules/better_print.py
+++ modules/better_print.py
@@ -9,35 +9,35 @@ def print_with_lock(text):
     print(text)
     print_lock.release()
 
-def info(text):
-    print_with_lock(
-        f"({Fore.LIGHTYELLOW_EX}*{Fore.RESET}) {Fore.YELLOW}{text}{Fore.RESET}"
-    )
-def content(text, content):
-    print_with_lock(
-        f"({Fore.LIGHTGREEN_EX}+{Fore.RESET}) {text}: {Fore.GREEN}{content}{Fore.RESET}"
-    )
 
-def success(text, content):
-    print_with_lock(
-        f"({Fore.LIGHTBLUE_EX}+{Fore.RESET}) {text}: {Fore.BLUE}{content}{Fore.RESET}"
-    )
-def error(text):
-    print_with_lock(
-        f"({Fore.LIGHTRED_EX}-{Fore.RESET}) {Fore.LIGHTRED_EX}{text}{Fore.RESET}"
-    )
-def fail(text):
-    print_with_lock(
-        f"({Fore.LIGHTRED_EX}-{Fore.RESET}) {Fore.RED}{text}{Fore.RESET}"
-    )
-
-def cinput(text, no=None):
-    if no != None:
-        data = input(
-            f"({Fore.LIGHTCYAN_EX}~{Fore.RESET}) {Fore.CYAN}{text}{Fore.RESET}" 
+class console():
+    def information(text):
+        print_with_lock(
+            f"({Fore.LIGHTYELLOW_EX}*{Fore.RESET}) {Fore.YELLOW}{text}{Fore.RESET}"
         )
-    else:
-        data = input(
-            f"({Fore.LIGHTCYAN_EX}~{Fore.RESET}) {Fore.CYAN}{text}{Fore.RESET} >> " 
+    def content(text, content):
+        print_with_lock(
+            f"({Fore.LIGHTGREEN_EX}+{Fore.RESET}) {text}: {Fore.GREEN}{content}{Fore.RESET}"
         )
-    return data
+    def success(text, content):
+        print_with_lock(
+            f"({Fore.LIGHTBLUE_EX}+{Fore.RESET}) {text}: {Fore.CYAN}{content}{Fore.RESET}"
+        )
+    def error(text):
+        print_with_lock(
+            f"({Fore.LIGHTRED_EX}-{Fore.RESET}) {Fore.LIGHTRED_EX}{text}{Fore.RESET}"
+        )
+    def failure(text):
+        print_with_lock(
+            f"({Fore.LIGHTRED_EX}-{Fore.RESET}) {Fore.RED}{text}{Fore.RESET}"
+        )
+    def input(text, custom=None):
+        if custom != None:
+            data = input(
+                f"({Fore.LIGHTCYAN_EX}~{Fore.RESET}) {Fore.CYAN}{text}{Fore.RESET}" 
+            )
+        else:
+            data = input(
+                f"({Fore.LIGHTCYAN_EX}~{Fore.RESET}) {Fore.CYAN}{text}{Fore.RESET} >> " 
+            )
+        return data
